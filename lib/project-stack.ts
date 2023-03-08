0a0bcd8a0a7ca0d675f1cc8efe42da003dd6244f
Mahmut Bayri <mahmutbayri@yahoo.com>
2023-03-08 22:57:06 +0300

    first commit

--- lib/project-stack.ts
+++ lib/project-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,9 @@ export class ProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // new s3.Bucket(this, 'MyFirstBucket', {
+    //   versioned: true,
+    //   enforceSSL: true,
     // });
   }
 }
